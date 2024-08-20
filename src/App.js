f35899761be8872e07851d38d0ba2fc4640529eb
Lee Yao Yang <yylee.work@gmail.com>
2024-08-20 14:18:07 +1000

    Testing azure devops project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+// This is a test
 function App() {
   return (
     <div className="App">
