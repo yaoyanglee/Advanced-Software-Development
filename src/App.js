e6facf0c3dc2440cfbad6529bb0c1af719a3c628
Lee Yao Yang <yylee.work@gmail.com>
2024-08-27 20:09:29 +1000

    Connected to database

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
+import RegisterProperty from "./pages/RegisterProperty";
+
 // import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="max-w-[1440px] mx-auto bg-white ">
-      <Header />
+      {/* Uncomment this code to see how we can query the database */}
+      <RegisterProperty />
+
+      {/* Uncomment this code below to get back to the normal functionality */}
+      {/* <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
