fe0313b649a889ab5fc7dadbf873ff4c17041cbc
JYE01 <160466441+JYE01@users.noreply.github.com>
2024-09-24 14:23:25 +1000

    toggle view added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,12 @@ import Search from "../components/Search";
 import Navbar from "../components/Navbar";
 import RentPropertyModal from "./RentPropertyModal";
 import SellPropertyModal from "./SellPropertyModal";
+import "./Home.css";
 
 const Home = () => {
   const [rentModal, setRentModal] = useState(false);
   const [sellModal, setSellModal] = useState(false);
+  const [isToggled, setToggle] = useState(true);
 
   const toggleRentModal = () => {
     setRentModal(!rentModal);
@@ -18,19 +20,30 @@ const Home = () => {
     setSellModal(!sellModal);
   };
 
+  const toggleButton = () => {
+    setToggle(!isToggled);
+  };
+
   return (
     <div className="min-h-screen">
       <Navbar
         toggleRentModal={toggleRentModal}
         toggleSellModal={toggleSellModal}
       />
-      <RentPropertyModal modal={rentModal} toggleModal={toggleRentModal} />
-      <SellPropertyModal modal={sellModal} toggleModal={toggleSellModal} />
+      {/* <RentPropertyModal modal={rentModal} toggleModal={toggleRentModal} />
+      <SellPropertyModal modal={sellModal} toggleModal={toggleSellModal} /> */}
     
       <Search />
-      <br></br>
-      {/* <HouseList /> */}
-      <HouseMap />
+      <br />
+      <div className="toggle-switch">
+        <label className="switch">
+          <input type="checkbox" onChange={toggleButton} checked={isToggled} />
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <div className="content-container">
+        {isToggled ? <HouseList /> : <HouseMap />}
+      </div>
     </div>
   );
 };
